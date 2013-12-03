8a3112c82050d4abb43fdad932da197a934cee4a
Kinnell <kinnell@gmail.com>
2013-12-03 10:56:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adonis::Application.config.secret_key_base = '26ed2e0cdf0767c5592e0eba7bda6cdc6937b7c5ec5613f880ca35addf32f5282dcc8fc5ce6d6dd673455f03b4313ac6ee6df499af2f2250a8b509e3ef4f04b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Adonis::Application.config.session_store :cookie_store, key: '_adonis_session'
