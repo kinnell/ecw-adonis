8018d22f64e12fb7da77b88065973bd3333075f2
Kinnell <kinnell@gmail.com>
2013-12-11 15:30:05 -0500

    Pushing to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :email_regexp => /\A[\w+\-.]+@eclinicalworks.com/i
 
-
-   
    validates_format_of :email, :with=>email_regexp, :allow_blank => true, :message=>"needs to be an eClinicalWorks email."      
 
    has_many :weighins
