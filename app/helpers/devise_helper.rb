3c9951dba212bc84862c8bb43cca3a0b91c60da2
Kinnell <kinnell@gmail.com>
2013-12-27 08:10:29 -0500

    Noty for devise error messages

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,17 +1,9 @@
 module DeviseHelper
+
+
   def devise_error_messages!
-    return '' if resource.errors.empty?
+      return "" if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:div, msg, class: "alert alert-danger alert-dismissable") }.join
-    sentence = I18n.t('errors.messages.not_saved',
-      count: resource.errors.count,
-      resource: resource.class.model_name.human.downcase)
-
-    html = <<-HTML
-
-        #{messages}
-    HTML
-
-    html.html_safe
+      return flash[:notice].to_a.concat resource.errors.full_messages
   end
 end
