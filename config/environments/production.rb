918b2dfa6e9c2259c25e1c6ec8940b673fb177d2
Kinnell <kinnell@gmail.com>
2014-01-02 11:51:28 -0500

    Forgetting Password Active Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,12 @@ Adonis::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'http://ecw-adonis.herokuapp.com/' }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'kinnell@gmail.com'}
+  config.action_mailer.default_url_options = { :host => 'http://www.ecwbiggestloser.com/' }
+
+
+
 end
